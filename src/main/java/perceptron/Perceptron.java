c0964f63ed47a556898e618824fc63593dfe8bd0
Marcin Styczen <test@github.com>
2015-05-11 13:04:04 +0200

    1 commit.

---
+++ src/main/java/perceptron/Perceptron.java
@@ -0,0 +1,11 @@
+package perceptron;
+
+/**
+ * Created by Marcin on 2015-05-11.
+ */
+public class Perceptron {
+
+
+
+
+}
---
+++ src/main/java/perceptron/Start.java
@@ -0,0 +1,11 @@
+package perceptron;
+
+/**
+ * Created by Marcin on 2015-05-11.
+ */
+public class Start {
+    public static void main(String[] args) {
+        
+    }
+
+}
